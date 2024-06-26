4bc174a5384d0d90454fe1a3983b1cb07dd98105
Icham Jaber <ijaber@student.42.fr>
2024-06-26 17:25:22 +0200

    update

--- so_long_destroyer.sh
+++ so_long_destroyer.sh
@@ -4,6 +4,19 @@ make re -C ../
 
 clear
 
-for map in bad_map/*.ber; do
-    valgrind .././so_long "$map"
-done
+valgrind --leak-check=full ./so_long maps/map2.ber > valgrind_output.txt 2>&1
+
+if grep -q "All heap blocks were freed -- no leaks are possible" valgrind_output.txt; then
+    LEAKS=0
+else
+    LEAKS=1
+fi
+
+if [ $LEAKS -eq 1 ]; then
+    printf "${RED}$C.[MKO] LEAKS ${DEF_COLOR}\n"
+    cat valgrind_output.txt
+else
+    printf "${GREEN}$C.[MOK]${DEF_COLOR}\n"
+fi
+
+rm valgrind_output.txt
